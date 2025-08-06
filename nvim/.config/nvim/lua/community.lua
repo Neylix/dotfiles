51323a7f3a6c6964cbc70c5d752da3623aaf6108
Neylix <julien.leclerc05@protonmail.com>
2025-08-06 12:41:10 +0200

    Update markdown plugins

--- nvim/.config/nvim/lua/community.lua
+++ nvim/.config/nvim/lua/community.lua
@@ -8,5 +8,6 @@ return {
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.dart" },
   { import = "astrocommunity.utility.noice-nvim" },
+  { import = "astrocommunity.markdown-and-latex.render-markdown-nvim" },
   -- import/override with your plugins folder
 }
