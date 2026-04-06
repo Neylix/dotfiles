e02473654ce0dfdead5080ba2c0b580559a03d93
Neylix <julien.leclerc05@protonmail.com>
2026-04-06 23:50:18 +0200

    Update astronvim to v6

--- nvim/.config/nvim/lua/plugins/disabled.lua
+++ nvim/.config/nvim/lua/plugins/disabled.lua
@@ -1,4 +1,9 @@
 return {
-  -- { "folke/which-key.nvim", enabled = false },
   { "AstroNvim/astrotheme", enabled = false },
+  { "akinsho/toggleterm.nvim", enabled = false },
+  { "stevearc/resession.nvim", enabled = false },
+  { "jay-babu/mason-nvim-dap.nvim", enabled = false },
+  { "mfussenegger/nvim-dap", enabled = false },
+  { "rcarriga/cmp-dap", enabled = false },
+  { "rcarriga/nvim-dap-ui", enabled = false },
 }
--- nvim/.config/nvim/lua/plugins/onedark.lua
+++
@@ -1,5 +0,0 @@
----@type LazySpec
-return {
-  "navarasu/onedark.nvim",
-  name = "onedark",
-}
